f6b058da40f117936704eb211ca65f9acc4a8d5d
Mhyssa <jolym8@gmail.com>
2023-12-20 12:55:24 +0100

    :seedling: Read ingredient before paginator bundle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,8 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         
-        return $this->render('home.html.twig');
+        return $this->render('pages/home.html.twig');
+    
     }
     // Todo
 }
