b09d77a56e26e41d76b1297d7fc7be419199d5ad
Mhyssa <jolym8@gmail.com>
2024-01-26 12:02:39 +0100

    :sparkles: Add MailService

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,26 +4,32 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use App\Service\MailService;
 
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\HttpFoundation\Request;
+
+
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact.index')]
-    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailService $mailService): Response
     {
-
+        
         $contact = new Contact();
         
         if ($user = $this->getUser()) {
+
+        /**
+        * @var UserEntity
+        */
+        $user = $this->getUser();
+        
             $contact->setFullName($user->getFullName())
                 ->setEmail($user->getEmail());
         }
@@ -39,20 +45,13 @@ class ContactController extends AbstractController
             $manager->flush();
 
             //Email
-            $email = (new TemplatedEmail())
-            ->from($contact->getEmail())
-            ->to('admin@symrecipe.com')
-            ->subject($contact->getSubject())
-            // path of the Twig template to render
-            ->htmlTemplate('emails/contact.html.twig')
-            // pass variables (name => value) to the template
-            ->context([
-                'contact' => $contact,
-                'expiration_date' => new \DateTime('+7 days'),
-            ])
-            ;
+            $mailService->sendEmail(
+                $contact->getEmail(),
+                $contact->getSubject(),
+                'emails/contact.html.twig',
+                ['contact' => $contact]
+            );
 
-            $mailer->send($email);
 
             $this->addFlash(
                 'success',
