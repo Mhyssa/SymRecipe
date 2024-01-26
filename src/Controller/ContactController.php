c32e6ba97523b8b598d08cdff255128762ec2b1e
Mhyssa <jolym8@gmail.com>
2024-01-26 01:20:12 +0100

    :sparkles: Add Email Sender with mailtrap and reCaptcha3

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,10 +11,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact.index')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
 
         $contact = new Contact();
@@ -33,6 +37,23 @@ class ContactController extends AbstractController
 
             $manager->persist($contact);
             $manager->flush();
+
+            //Email
+            $email = (new TemplatedEmail())
+            ->from($contact->getEmail())
+            ->to('admin@symrecipe.com')
+            ->subject($contact->getSubject())
+            // path of the Twig template to render
+            ->htmlTemplate('emails/contact.html.twig')
+            // pass variables (name => value) to the template
+            ->context([
+                'contact' => $contact,
+                'expiration_date' => new \DateTime('+7 days'),
+            ])
+            ;
+
+            $mailer->send($email);
+
             $this->addFlash(
                 'success',
                 'Votre demande à bien été prise en compte');
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,15 +4,17 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+
 use Symfony\Component\Form\FormBuilderInterface;
-
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
 
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 
 class ContactType extends AbstractType
 {
@@ -81,6 +83,10 @@ class ContactType extends AbstractType
             ],
             'label'=>'Envoyer mon message'
         
+        ])
+        ->add('captcha', Recaptcha3Type::class, [
+            'constraints' => new Recaptcha3(),
+            'action_name' => 'contact',
         ])
         ;
     }
